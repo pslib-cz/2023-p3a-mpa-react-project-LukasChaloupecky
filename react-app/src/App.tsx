4bde5104f3ac1ab9e6be255c6ac6389e80efa56b
LukasChaloupecky <lukyn319@gmail.com>
2024-04-01 22:01:12 +0200

    adding page

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
+import { useReducer, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Registration from './pages/Registration'
+import GameState, { GameContext, initialGameState } from './components/GameState'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, dispatch] = useReducer(GameState, initialGameState)
 
   return (
     <>
-     
+      {state.isRegistered ? (
+        <>
+          {state.person.firstName}
+        </>
+      ) : (
+        <GameContext.Provider value={{ state: state, dispatch: dispatch }}>
+          <Registration></Registration>
+        </GameContext.Provider>
+      )}
     </>
   )
 }
--- react-app/src/components/GameState.tsx
+++ react-app/src/components/GameState.tsx
@@ -1,6 +1,8 @@
+import { createContext } from "react";
+
 // GameState.tsx
-
-
+//export const GameContext = createContext<GameState | undefined>(undefined);
+export const GameContext = createContext<{ state: GameState; dispatch: React.Dispatch<Action> } | undefined>(undefined);
 type Armor = {
     id: number;
     name: string;
@@ -9,11 +11,16 @@ type Armor = {
     price: number;
     defense: number;
 };
-
+type Person = { 
+    firstName: string;
+    lastName: string;
+}
 
 // Define the initial state of the game
 type GameState = { 
+    isRegistered : boolean;
     lives: number;
+    person : Person;
     score: number;
     money: number;
     Armors: Armor[];
@@ -22,8 +29,10 @@ type GameState = {
 };
 
 
-const initialGameState : GameState = {
+export const initialGameState : GameState = {
+    isRegistered : false,
     lives: 3,
+    person : { firstName: '', lastName: ''},
     score: 0,
     money: 0,
     Armors : [ ],
@@ -33,19 +42,28 @@ const initialGameState : GameState = {
 
 // Define the action types
 type Action =
-    | { type: 'INCREMENT_LIVES'; payload: number }
-    | { type: 'DECREASE_LIVES'; payload: number}
-    | { type: 'GAME_OVER' };
+    | { type: 'INCREMENT_LIVES', payload: number }
+    | { type: 'DECREASE_LIVES', payload: number}
+    | { type: 'MOVE_PLAYER', payload: number}
+    | {type : 'UPDATE_PERSON', payload : Person}
+    | { type: 'GAME_OVER' }
+    | {type : "IS_REGISTERED", payload : boolean};
 
 // Define the reducer function
-const GameState = (state = initialState, action: Action) => {
+const GameState = (state = initialGameState, action: Action) => {
     switch (action.type) {
         case 'INCREMENT_LIVES':
             return { ...state, lives: state.lives + action.payload };
         case 'DECREASE_LIVES':
             return { ...state, lives: state.lives - action.payload};
+        case 'MOVE_PLAYER':
+            return { ...state, CurrentSpot: state.CurrentSpot + action.payload};
         case 'GAME_OVER':
             return { ...state, lives: 0 };
+        case 'UPDATE_PERSON':
+            return { ...state, person : action.payload};
+        case 'IS_REGISTERED':
+            return { ...state, isRegistered : action.payload};
         default:
             return state;
     }
