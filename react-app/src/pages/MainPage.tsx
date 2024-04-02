ff503d5c8fbe747f5e72158dc2257bd6d7fb97b1
LukasChaloupecky <lukyn319@gmail.com>
2024-04-02 10:26:15 +0200

    added css file

--- react-app/src/pages/MainPage.tsx
+++ react-app/src/pages/MainPage.tsx
@@ -4,13 +4,19 @@ import MainPanel from './pageComponents/MainPanel';
 //import { DataProvider } from '../components/DataProvider';
 import Board from './subpage/Board';
 import { GetEnemyCollection } from '../components/methods/EnemyFunctions';
+import Shop from './subpage/Shop';
 
 export const MainPage: React.FC = () => {
     const Reducer = useState(GameContext);
     //const Data = useState(DataProvider);
 
     // TODO: Figure out a valid approach (= playable) as parameters for GetEnemyCollection
-    const Enemies : EnemyBoard[] = GetEnemyCollection({boardSize: 10, boardDifficulty: 50, minEnemyDifficulty: 1, maxEnemyDifficulty: 10}); // ? these params are temporary here
+    const [Enemies, setEnemies] = useState<undefined | EnemyBoard[]>(); // ? these params are temporary here
+
+
+    const createBoard = () => {
+        setEnemies(GetEnemyCollection({boardSize: 10, boardDifficulty: 50, minEnemyDifficulty: 1, maxEnemyDifficulty: 10}));
+    };
 
     // TODO_0: Create a valid collection of enemies on the board
     // TODO_0: Create a valid collection of benefits on the board
@@ -21,6 +27,7 @@ export const MainPage: React.FC = () => {
     return (
         <div className='mainPage'>
             <Board />
+            <Shop />
         </div>
 
         
--- react-app/src/pages/subpage/Board.tsx
+++ react-app/src/pages/subpage/Board.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import MainPanel from '../pageComponents/MainPanel';
 import { GetEnemyCollection } from '../../components/methods/EnemyFunctions';
+import '../../styles/board.css'; 
 
 interface BoardProps {
-    // Add any props you need for the Board component
 }
 
 
 // ? The Gaming Page
 const Board: React.FC<BoardProps> = () => {
-    
+    // ? BOARD IS 15x7
     return (
         <div>
             <MainPanel />
+            <div className='boardTop'> 
+                <div className='chest'>
+                    <img alt='store icon' />
+                    <p>Go to SHOP</p>
+                </div>
+                <div className='board'>
+                    
+                </div>
+            </div>
         </div>
     );
 };
